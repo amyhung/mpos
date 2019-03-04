7a7ba3ae73a497d165503b6b7b3d8b9f169f3318
Amy Hung <amyhung@transglobe.com.tw>
2019-03-05 01:09:54 +0800

    deploy to jboss study

--- mpos-api/src/main/java/com/tgl/mpos/api/MposApiApplication.java
+++ mpos-api/src/main/java/com/tgl/mpos/api/MposApiApplication.java
@@ -2,14 +2,21 @@ package com.tgl.mpos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.tgl.mpos")
-public class MposApiApplication {
+public class MposApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MposApiApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MposApiApplication.class);
 	}
 
 }
