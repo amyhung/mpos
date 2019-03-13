283f2b02a320bfda618b123224fea1fd5f4bb446
Amy Hung <amyhung@transglobe.com.tw>
2019-03-14 01:34:28 +0800

    加入不用 xml，用 annotation 的方式寫 mapper

--- mpos-dao/src/main/java/com/tgl/mpos/dao/mapper/CodeItemDefMapper.java
+++ mpos-dao/src/main/java/com/tgl/mpos/dao/mapper/CodeItemDefMapper.java
@@ -1,13 +1,19 @@
 package com.tgl.mpos.dao.mapper;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.tgl.mpos.dao.model.CodeItemDef;
 
 public interface CodeItemDefMapper {
 	
-	//@Select("select * from code_item_def where item_code =#{code}")
+	@Select("select * from code_item_def where item_code =#{code}")
+	@Results({
+		@Result(property = "itemCode",  column = "item_code"),
+		@Result(property = "itemDesc", column = "item_desc")
+	})
 	CodeItemDef getByCode(@Param("code") String code);
 
 }
